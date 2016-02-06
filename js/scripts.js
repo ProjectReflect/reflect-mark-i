605f6b23b78fc581a38a2791fff5271eb83d6c1c
Tim Mui <timmui123@gmail.com>
2016-02-05 22:46:54 -0500

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,120 @@
+var page = 1;
+
+function startMirror() {
+	page = 1;
+	$( "#page-2" ).hide();
+	$( "#page-1" ).fadeIn( "slow", null);
+}
+function stopMirror() {
+	$( "#page-1" ).fadeOut( "slow", null);
+	$( "#page-2" ).fadeOut( "slow", null);
+	$( "#time-and-date" ).fadeOut( "slow", null);
+}
+
+startMirror();
+//stopMirror();
+
+
+//the clock and date
+function updateClock() {
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	var d = new Date();
+	document.getElementById("date").innerHTML = days[d.getDay()]+ ', ' + months[d.getMonth()] + ' ' + d.getDate() + ', ' + d.getFullYear();
+	var min = d.getMinutes();
+	if (min < 10) {
+		min = "0"+min;
+	}
+	var hour = d.getHours();
+    
+    if(hour < 12) {
+		document.getElementById("welcome").innerHTML = "Good Morning, Tim!"
+	} else if (hour < 15) {
+		document.getElementById("welcome").innerHTML = "Good Afternoon, Tim!"
+	} else if (hour < 22) {
+		document.getElementById("welcome").innerHTML = "Good Evening, Tim!"
+	} else {
+		document.getElementById("welcome").innerHTML = "Good Night, Tim!"
+	}
+    
+    // 12 hour clock
+    if (hour > 12){
+        hour = hour-12;
+        document.getElementById("am-pm").innerHTML = "PM";
+    }
+    else{
+        document.getElementById("am-pm").innerHTML = "AM";
+    }
+    
+	document.getElementById("time").innerHTML = hour + ":" + min;
+	
+    // call this function again in 1000ms
+    setTimeout(updateClock, 1000);
+}
+updateClock(); // initial call
+
+ //weather
+function updateWeather(){
+    $.get("http://api.openweathermap.org/data/2.5/weather?lat=43.4667&lon=-80.5167&appid="+ owmKey, function(data, status){
+        document.getElementById("temperature").innerHTML = Math.round( (data.main.temp - 273.15) * 10)/10;
+        var minTemp = Math.round( (data.main.temp_min-273.15) * 10)/10;
+        var maxTemp = Math.round( (data.main.temp_max-273.15) * 10)/10;
+        //document.getElementById("max-min").innerHTML = "  "+ minTemp + "°|" + maxTemp+"°";
+        console.log(data + " " + status);
+        document.getElementById("weatherDesc").innerHTML = data.weather[0].description;
+        document.getElementById("city").innerHTML = data.name;
+    });
+    // call this function again
+    setTimeout(updateWeather, 1200000);
+}
+updateWeather(); // initial call
+
+//eta
+$.get(server + "/api/eta?userKey="+userKey, function(data, status){
+	var data = JSON.parse(data);
+	document.getElementById("eta").innerHTML = data.routes[0].legs[0].duration.text;
+    document.getElementById("etaHeading").innerHTML = "ETA [TO WORK]";
+});
+
+//news
+$.get(server + "/api/news?userKey="+userKey, function(data, status){
+	var jsonObj = JSON.parse(data);
+	console.log(jsonObj);
+	document.getElementById("news1").innerHTML = jsonObj[0].title;
+	document.getElementById("news2").innerHTML = jsonObj[1].title;
+	document.getElementById("news3").innerHTML = jsonObj[2].title;
+    document.getElementById("newsHeading").innerHTML = "NEWS";
+});
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
---
+++ js/secret.js
@@ -0,0 +1,4 @@
+var owmKey = "OpenWeatherMap Key";
+var name = "Name";
+var userKey = "User Key";
+var server = "Server base address";
