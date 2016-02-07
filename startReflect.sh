95553005c88dffaa5110f6386daa7bb7b05e7291
Tim Mui <timmui123@gmail.com>
2016-02-07 02:53:19 -0500

    Added wait for internet in startup script

--- startReflect.sh
+++ startReflect.sh
@@ -2,7 +2,18 @@
 
 cd ~/reflect-mark-i/
 git stash
-sleep 5
+
+# Check for internet
+((count = 100))                            # Maximum number to try.
+while [[ $count -ne 0 ]] ; do
+    ping -c 1 8.8.8.8                      # Try once.
+    rc=$?
+    if [[ $rc -eq 0 ]] ; then
+        ((count = 1))                      # If okay, flag to exit loop.
+    fi
+    ((count = count - 1))                  # So we don't go forever.
+done
+
 git pull
 git stash pop
 python -m SimpleHTTPServer
