e939df48fddccbb0c41c1d4e609aba68cd4fa3a9
Tim Mui <timmui123@gmail.com>
2016-02-07 03:06:44 -0500

    Revert "Added wait for internet in startup script"
    This reverts commit 95553005c88dffaa5110f6386daa7bb7b05e7291.

--- startReflect.sh
+++ startReflect.sh
@@ -2,18 +2,7 @@
 
 cd ~/reflect-mark-i/
 git stash
-
-# Check for internet
-((count = 100))                            # Maximum number to try.
-while [[ $count -ne 0 ]] ; do
-    ping -c 1 8.8.8.8                      # Try once.
-    rc=$?
-    if [[ $rc -eq 0 ]] ; then
-        ((count = 1))                      # If okay, flag to exit loop.
-    fi
-    ((count = count - 1))                  # So we don't go forever.
-done
-
+sleep 5
 git pull
 git stash pop
 python -m SimpleHTTPServer
